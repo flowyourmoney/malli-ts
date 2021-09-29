589386a8ebb8f11a107a89b77ff8dbe6a2a4ee60
Tiago Dall'Oca <tdalloca@gmail.com>
2021-09-29 13:52:57 -0300

    testing docstring

--- tests/malli_ts/tests/core.cljs
+++ tests/malli_ts/tests/core.cljs
@@ -1,10 +1,10 @@
 (ns malli-ts.tests.core
   (:require [cljs.test :refer-macros [deftest is run-tests testing]]
             [malli-ts.ast :refer [->ast]]
-            [malli-ts.core :refer [-parse-ast-node parse-files]]
+            [malli-ts.core :refer [-parse-ast-node parse-files ->type-declaration-str -jsdoc-literal]]
             [malli.core :as m]))
 
-(deftest parse-functions
+(deftest function-types
   (is (= (-parse-ast-node
           (->ast [:=> [:cat :string :int] [:map [:a :int] [:b :string]]]))
          "(a:string, b:number) => {\"a\":number,\"b\":string}"))
@@ -20,5 +20,15 @@
           (->ast [:=> [:cat] any?]))
          "() => any")))
 
+(deftest declaration
+  (let [schema [:map-of :string :any]
+        literal (-parse-ast-node (->ast schema))
+        jsdoc-literal (-jsdoc-literal [["schema" (str schema)]
+                                       ["author" "Mr. Poopybutthole"]])
+        declaration-str (->type-declaration-str "config" literal jsdoc-literal
+                                                {:export true :declare true})]
+    (is (= declaration-str
+           "/**\n * @schema [:map-of :string :any]\n * @author Mr. Poopybutthole\n */\nexport var config: {[k:string]:any};"))))
+
 (testing "ast parsing"
-  (parse-functions))
+  (function-types))
